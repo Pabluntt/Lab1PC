d441a5d906f9acbcfc8b1d0febac845e35ecd937
Pabluntt <pablo.lopez.saavedra3001@gmail.com>
2025-10-21 22:51:05 -0300

    TD-IDF y nombre en el readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab1PC
 Laboratorio 1 de programación científica
+
+Pablo López Saavedra 21.518.678-4
--- main.py
+++ main.py
@@ -27,13 +27,35 @@ def normalizar_texto(texto: str) -> str:
     texto = re.sub(r'\s+', ' ', texto).strip()
     return texto
 
+# Separa el texto en palabras individuales
 def tokenizar(texto: str) -> List[str]:
     if not texto:
         return []
     return texto.split()
 
+# Elimina stopwords
 def remover_stopwords(tokens: List[str]) -> List[str]:
     return [t for t in tokens if t not in stopwords and len(t) > 1]
 
+# Ejecuta el preprocesamiento, quitando stopwords, tokenizando y normalizando
 def preprocesamiento(texto: str) -> List[str]:
     return remover_stopwords(tokenizar(normalizar_texto(texto)))
+
+# Calcula la frecuencia por termino, devuelve un diccionario con cada string y su frecuencia
+def calcular_tf(tokens: List[str]) -> Dict[str, float]:
+    cnt = Counter(tokens)
+    n = len(tokens) or 1
+    return {t: c / n for t, c in cnt.items()}
+
+# Calcula el IDF de cada término
+def calcular_idf(corpus_tokens: List[List[str]]) -> Dict[str, float]:
+    N = len(corpus_tokens)
+    df = Counter()
+    for tokens in corpus_tokens:
+        df.update(set(tokens))
+    return {t: math.log((N + 1) / (df_t + 1)) + 1 for t, df_t in df.items()}
+
+# Calcula el vector, recibiendo los tokens y el diccionario con los IDF
+def vector_tfidf(tokens: List[str], idf: Dict[str, float]) -> Dict[str, float]:
+    tf = calcular_tf(tokens)
+    return {t: tf_val * idf.get(t, 0.0) for t, tf_val in tf.items()}
